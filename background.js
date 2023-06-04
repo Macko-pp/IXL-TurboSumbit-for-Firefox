0dc6c44188610d6bed5d8d20a679331ece47c6d6
Macko++ <mackomaxo@duck.com>
2023-06-03 22:47:04 -0500

    Now uses the toolbar instead of a context menu

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
-browser.contextMenus.create({
-	id: "cheat",
-	title: "Answer ~3 questions (30 mins)"
-});
+// browser.contextMenus.create({
+// 	id: "cheat",
+// 	title: "Answer ~3 questions (30 mins)"
+// });
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
-  	if (info.menuItemId === "cheat") {
+browser.browserAction.onClicked.addListener((info, tab) => {
+  	// if (info.menuItemId === "cheat") {
     	browser.tabs.executeScript({
       		file: "ixlCheat.js"
     	});
-  	}
+  	// }
 });
